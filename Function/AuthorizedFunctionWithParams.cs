4216740384e7ab797b2f2538534815573d0438e6
radekRadekk <radek@radek.com>
2022-10-13 11:09:23 +0200

    New function

---
+++ Function/AuthorizedFunctionWithParams.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Function;
+
+public static class AuthorizedFunctionWithParams
+{
+    private record Player(string Name, string Surname, string Country);
+
+    [FunctionName("AuthorizedFunctionWithParams")]
+    public static async Task<IActionResult> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+        HttpRequest req, ILogger log)
+    {
+        var players = new[]
+        {
+            new Player("John", "Brown", "USA"),
+            new Player("Sasza", "Smirnov", "RUS"),
+            new Player("Dima", "Denaturov", "RUS"),
+            new Player("Adam", "Malysz", "PL"),
+            new Player("Kamil", "Stoch", "PL")
+        };
+
+        log.LogInformation("C# HTTP trigger function processed a request.");
+
+        string country = req.Query["country"];
+
+        return country != null
+            ? (ActionResult) new OkObjectResult(players.Where(p => p.Country == country.ToUpper()).ToArray())
+            : new BadRequestObjectResult("Please pass a country on the query string or in the request body");
+    }
+}
